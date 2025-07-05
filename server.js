9c1b31a20cb8a3f17d8d029866e1bdd5742d99e0
nikhil-os <nikhilsahu.prgmail.com>
2025-07-06 01:22:27 +0530

    refactor CORS configuration to simplify usage and remove unnecessary options

--- server.js
+++ server.js
@@ -5,15 +5,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+app.use(cors());
+
 const app = express();
 app.use(express.json());
 
 const allowedOrigins = ['https://rentx-git-master-nikhil-sahus-projects-0bc03434.vercel.app/' || 'https://rentx-phi.vercel.app/'];
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
 app.options('/{*any}', cors()); // Handle preflight for all routes
 
 app.use(express.urlencoded({ extended: true }));
